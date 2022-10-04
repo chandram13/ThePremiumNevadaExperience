29b408d04b81ac8c1b51f4bc57df4f1e07b0ebc5
Marvish Subhas Chandra <mschandr@uci.edu>
2022-10-04 12:08:46 -0700

    Add files via upload

---
+++ main.java
@@ -0,0 +1,220 @@
+// Marvish Chandra
+// Exploring the Venetian Resort
+
+public class venetianSuites{
+    int premiumtwoqueensuitelength;
+    string premiumtwoqueensuiterooms;
+
+    int grandkingsuitelength;
+    string grandkingsuiterooms;
+
+    int granndonebedroomsuitelength;
+    string granndonebedroomsuiterooms;
+
+    int executivekingsuitelength;
+    string executivekingsuiterooms;
+
+    int penthousesuitelength;
+    string penthousesuiterooms;
+
+    int presidentialsuitelength;
+    string presidentialsuiterooms;
+
+    int chairmansuitelength;
+    string chairmansuiterooms;
+
+
+    static void venetianChoices(String[] args){
+        venentianSuites myVenetianSuites = new venetianSuites;
+        myVenetianSuites.premiumtwoqueensuitelength = 1100;
+        myVenetianSuites.premiumtwoqueensuiterooms = "2 Queen Beds";
+
+        myvenentianChoices.grandkingsuitelength = 1500;
+        myvenentianChoices.grandkingsuiterooms = "1 King Bed";
+
+        myvenentianChoices.granndonebedroomsuitelength = 1300;
+        myvenentianChoices.granndonebedroomsuiterooms = "1 King Bed";
+
+        myvenentianChoices.executivekingsuitelength = 1850;
+        myvenentianChoices.executivekingsuiterooms = "1 King Bed";
+
+        myvenentianChoices.penthousesuitelength = 2600;
+        myvenentianChoices.penthousesuiterooms = "2 Bedrooms";
+
+        myvenentianChoices.presidentialsuitelength = 5200;
+        mypresidentialsuiterooms = "2 Bedrooms";
+
+        myvenentianChoices.chairmansuitelength = 8000 - 9000;
+        myvenentianChoices.chairmansuiterooms = "3 - 4 Bedrooms";
+    }
+}
+
+public class palazzoSuites{
+    int premiumkingsuitelength;
+    string premiumkingsuiterooms;
+
+    int grandonebedroomsuitelength;
+    string grandonebedroomsuiterooms;
+
+    int executivesuiteslength;
+    string executivesuitesrooms;
+
+    int penthousesuitelength;
+    string penthousesuiterooms;
+
+    int presidentialsuitelength;
+    string presidentialsuiterooms;
+
+    int chairmansuitelength;
+    string chairmansuiterooms;
+
+    static void palazzoChoices{
+        palazzoSuites mypalazzoChoices = new palazzoChoices;
+        mypalazzoSuites.premiumkingsuitelength = 940;
+        mypalazzoSuites.premiumkingsuiterooms = "1 King Bed";
+
+        mypalazzoSuites.grandonebedroomsuitelength = 1280;
+        mypalazzoSuites.grandonebedroomsuiterooms = "1 King Bed/2 Queen Beds";
+
+        mypalazzoSuites.executivesuiteslength = 1850;
+        mypalazzoSuites.executivekingsuiterooms = "1-2 Bedrooms";
+
+        mypalazzoSuites.penthousesuitelength = 3204;
+        mypalazzoSuites.penthousesuiterooms = "2 Bedrooms";
+
+        mypalazzoSuites.presidentialsuitelength = 6277;
+        mypalazzoSuites.presidentialsuiterooms = "3-4 Bedrooms";
+
+        mypalazzoSuites.chairmansuitelength = 6367;
+        mypalazzoSuites.chairmansuiterooms = "3-4 Bedrooms";
+    }
+}
+
+public class venetianRestaurants{
+    string MatteosRistoranteItaliano;
+    string Mott32;
+    string Bouchon;
+    string Yardbird;
+    string Cut;
+    string BlackTapCraftBurgersandBeer;
+    string BuddyVsRistorante;
+    string HaSalon;
+    string Miznon;
+    string LAVOItalianRestaurant;
+    string CapriPoolRestaurantandBar;
+    string SpritzRestaurantandBar;
+    string SUGARCANErawbargrill;
+    string SUSHISAMBA;
+    string TAOAsianBistro;
+    string DelmonicoSteakhouse;
+    string Canaletto;
+    string Canonita;
+    string CanyonRanchGrill;
+    string Casanova;
+    string CarlosBakery;
+    string CHICA;
+    string GrandLuxCafeatthePalazzo;
+    string GrandLuxCafeattheVentian;
+    string GrimladisPizzeria;
+    string HongKongCafe;
+    string MercatodellaPescheria;
+    string NoodleAsia;
+    string PrimeBurger;
+    string Rockhouse;
+    string RoyalBritanniaGastropub;
+    string SmithandWollensky;
+    string TheXPot;
+    string TrattoriaReggiano;
+    string TrustworthyBrewingCompany;
+    string TruthandTonic-WelnessCafe;
+    string ZepollaCafe;
+
+    static void choiceRestaurant{
+        venetianRestaurants mychoiceRestuarants = new choiceRestaurant;
+
+        myvenetianRestaurants.MatteosRistoranteItaliano = "ITALIAN, WEEKEND BRUNCH, LUNCH, DINNER, $$$";
+        myvenetianRestaurants.Mott32 = "ASIAN, DINNER, FINE DINING, $$$$";
+        myvenetianRestaurants.Bouchon = "FRENCH, LUNCH, DINNER, WEEKEND BRUNCH, FINE DINING, POOLSIDE DINING, PATIO DINING, $$$";
+        myvenetianRestaurants.Yardbird = "AMERICAN, LUNCH, DINNER, WEEKEND BRUNCH, CASUAL DINING, $$";
+        myvenetianRestaurants.CUT = "STEAKHOUSE, DINNER, FINE DINING, $$$$";
+        myvenetianRestaurants.BlackTapCraftBurgersandBeer = "AMERICAN, LUNCH, DINNER, CASUAL DINING, PATIO DINING, $$";
+        myvenetianRestaurants.BuddyVsRistorante = "ITALIAN, LUNCH, DINNER, HAPPY HOUR, CASUAL DINING, $$";
+        myvenetianRestaurants.HaSalon = "COMING SOON, FINE DINING, ISRAELI";
+        myvenetianRestaurants.Miznon = "COMING SOON, ISRAELI, VEGETARIAN, QUICK BITES";
+        myvenetianRestaurants.LAVOItalianRestaurant = "ITALIAN, DINNER, WEEKEND BRUNCH, FINE DINING, PATIO DINING, $$$";
+        myvenetianRestaurants.CapriPoolRestaurantandBar = "AMERICAN, BREAKFAST, LUNCH, CASUAL DINING, POOLSIDE DINING, PATIO DINING, $$";
+        myvenetianRestaurants.SpritzRestaurantandBar = "AMERICAN, BREAKFAST, LUNCH, CASUAL DINING, POOLSIDE DINING, PATIO DINING, $$";
+        myvenetianRestaurants.SUGARCANErawbargrill = "SEAFOOD, LATIN, GLOBAL CUISINE, BREAKFAST, LUNCH, DINNER, WEEKEND BRUNCH, HAPPY HOUR, LATE NIGHT, CASUAL DINING, $$$";
+        myvenetianRestaurants.SUSHISAMBA = "SEAFOOD, ASIAN, LUNCH, DINNER, LATE NIGHT, CASUAL DINING, $$$";
+        myvenetianRestaurants.TAOAsianBistro = "ASIAN, DINNER, LATE NIGHT, FINE DINING, $$$";
+        myvenetianRestaurants.DelmonicoSteakhouse = "STEAKHOUSE, LUNCH, DINNER, FINE DINING, $$$$";
+        myvenetianRestaurants.Canaletto = "ITALIAN, LUNCH, DINNER, HAPPY HOUR, CASUAL DINING, $$";
+        myvenetianRestaurants.Canonita = "MEXICAN, LATIN, LUNCH, DINNER, WEEKEND BRUNCH, CASUAL DINING, $$";
+        myvenetianRestaurants.CanyonRanchGrill = "AMERICAN, BREAKFAST, LUNCH, WEEKEND BRUNCH, CASUAL DINING, $$";
+        myvenetianRestaurants.Casanova = "GLOBAL CUISINE, ITALIAN, LUNCH, DINNER, CASUAL DINING, LATE NIGHT, $$";
+        myvenetianRestaurants.CarlosBakery = "ITALIAN, BREAKFAST, LUNCH, DINNER, CASUAL DINING, $$";
+        myvenetianRestaurants.CHICA = "LATIN, BREAKFAST, LUNCH, DINNER, WEEKEND BRUNCH, LATE NIGHT, FINE DINING, $$";
+        myvenetianRestaurants.GrandLuxCafeatthePalazzo = "AMERICAN, BREAKFAST, LUNCH, DINNER, HAPPY HOUR, LATE NIGHT, CASUAL DINING, $$";
+        myvenetianRestaurants.GrandLuxCafeattheVentian = "AMERICAN, BREAKFAST, LUNCH, DINNER, 24 HOUR DINING, CASUAL DINING, LATE NIGHT, $$";
+        myvenetianRestaurants.GrimladisPizzeria = "ITALIAN, LUNCH, DINNER, CASUAL DINING, $$";
+        myvenetianRestaurants.HongKongCafe = "ASIAN, CASUAL DINING, LUNCH, DINNER, LATE NIGHT, $$";
+        myvenetianRestaurants.MercatodellaPescheria = "SEAFOOD, CASUAL DINING, LUNCH, DINNER, ITALIAN, HAPPY HOUR, $$$";
+        myvenetianRestaurants.NoodleAsia = "ASIAN, CASUAL DINING, BREAKFAST, LUNCH, DINNER, LATE NIGHT, $$";
+        myvenetianRestaurants.PrimeBurger = "AMERICAN, LUNCH, DINNER, CASUAL DINING, LATE NIGHT, BREAKFAST, $$";
+        myvenetianRestaurants.Rockhouse = "LUNCH, DINNER, HAPPY HOUR, CASUAL DINING, AMERICAN, BREAKFAST, $$";
+        myvenetianRestaurants.RoyalBritanniaGastropub = "LUNCH, DINNER, HAPPY HOUR, CASUAL DINING, $$";
+        myvenetianRestaurants.SmithandWollensky = "STEAKHOUSE, LUNCH, DINNER, $$$$";
+        myvenetianRestaurants.TheXPot = "ASIAN, DINNER, LATE NIGHT, $$$$";
+        myvenetianRestaurants.TrattoriaReggiano = "ITALIAN, LUNCH, DINNER, CASUAL DINING, LATE NIGHT, $$";
+        myvenetianRestaurants.TrustworthyBrewingCompany = "AMERICAN, LUNCH, DINNER, WEEKEND BRUNCH, HAPPY HOUR, $$";
+        myvenetianRestaurants.TruthandTonic = "AMERICAN, BREAKFAST, LUNCH, WEEKEND BRUNCH, CASUAL DINING, $$";
+        myvenetianRestaurants.ZepollaCafe = "BREAKFAST, LUNCH, DINNER, ITALIAN, COFFEE, $$";
+    }
+}
+
+public class venetianEnterainment{
+    
+    string scheduledShow;
+
+    string nightlifeOption1;
+    string nightlifeOption2;
+    string nightlifeOption3;
+    string nightlifeOption4;
+    string nightlifeOption5;
+    string nightlifeOption6;
+    string nightlifeOption7;
+
+    static void venentianentertainmentChoices{
+        venetianEnterainment myvenentianentertainmentChoices = new venentianentertainmentChoices;
+
+        myvenetianEnterainment.scheduledShow = "Atomic Saloon Show";
+        
+        myvenetianEnterainment.nightlifeOption1. = "Bar Luca";
+        myvenetianEnterainment.nightlifeOption2. = "Bellini Bar";
+        myvenetianEnterainment.nightlifeOption3. = "The Cocktail Collective";
+        myvenetianEnterainment.nightlifeOption4. = "Electra Cocktail Club";
+        myvenetianEnterainment.nightlifeOption5. = "Rosina Cocktail Lounge";
+        myvenetianEnterainment.nightlifeOption6. = "The Dorsey Cocktail Bar";
+        myvenetianEnterainment.nightlifeOption7. = "TAO Nightclub";
+    }
+    
+}
+
+public class venetianMeetings{
+    string meetingspace1;
+    string meetingspace2;
+    string meetingspace3;
+    string meetingspace4;
+    string meetingspace5;
+
+    static void venetianmeetingChoices{
+        venetianMeetings myvenetianmeetingChoices = new venetianmeetingChoices;
+
+        myvenetianmeetingChoices.meetingspace1 = "The Stella Studio";
+        myvenetianmeetingChoices.meetingspace2 = "Hospitality Parlors and Suites";
+        myvenetianmeetingChoices.meetingspace3 = "Prestige Club Lounge";
+        myvenetianmeetingChoices.meetingspace4 = "Restaurants";
+        myvenetianmeetingChoices.meetingspace5 = "Theaters";
+
+    }
+}
